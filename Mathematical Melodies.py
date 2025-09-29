03ae4837ed8d5841c05b906b7f3e499f4905a3ba
NihalTakalkar <152111609+NihalTakalkar@users.noreply.github.com>
2025-09-28 19:35:09 -0500

    Mathematical Melodies.py

--- Mathematical Melodies.py
+++ Mathematical Melodies.py
@@ -1,14 +1,15 @@
 import math
-import numpy
-import pygame
+from mido import Message, Midifile, Miditrack
 
-pygame.init()
+mid = Midfile()
+track = Miditrack()
+mid.tracks.append(track)
 
-bits = 16
-sample_rate = 44100
-pygame.mixer.pre_init(sample_rate, )
+track.append(Message('note_on', note=60, velocity=64, time=0))
+track.append(Message('note_off', note=60, velocity=64, time=480))
+track.append(Message('note_on', note=64, velocity=64, time=0))
+track.append(Message('note_off', note=64, velocity=64, time=480))
+track.append(Message('note_on', note=67, velocity=64, time=0))
+track.append(Message('note_off', note=67, velocity=64, time=480))
 
-def sine_x(amp, freq, time):
-    return round(amp * math.sin(2 * math.pi * freq * time))
-
-
+mid.save('simple_song.mid')
